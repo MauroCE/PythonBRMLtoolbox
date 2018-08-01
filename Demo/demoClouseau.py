40007735d451e73bfc7dace7d05538bbf298d757
Mauro Camara Escudero <maurocamaraescudero@gmail.com>
2018-08-01 19:37:03 +0200

    corrected a few things in demo, following Variable class change

--- Demo/demoClouseau.py
+++ Demo/demoClouseau.py
@@ -1,14 +1,20 @@
 import numpy as np
 from brml.potential import Potential
-from brml.variables import Variable, order_variables, ix
+from brml.variables import Variable
 
 # Define variables
 butler = Variable(name='butler', index=2, domain=('murderer', 'notmurderer'))
 maid = Variable(name='maid', index=1, domain=('murderer', 'notmurderer'))
 knife = Variable(name='knife', index=0, domain=('used', 'notused'))
 
+# Define states
+murderer = 0
+notmurderer = 1
+used = 0
+notused = 1
+
 # Sort the variables by index
-variables = order_variables([butler, maid, knife])
+variables = Variable.order_variables([butler, maid, knife])
 
 # p(butler,maid,knife)=p(knife|butler,maid)p(butler)p(maid) therefore we need
 # 3 probability tables.
@@ -16,31 +22,30 @@ pot = []
 
 # p(butler) potential table (bt stands for butler table)
 bt = np.zeros(2)  # Only 2 states
-bt[ix(butler, 'murderer')] = 0.6
-bt[ix(butler, 'notmurderer')] = 0.4
+bt[butler.ix('murderer')] = 0.6
+bt[butler.ix('notmurderer')] = 0.4
 
 # p(maid) potential table (mt stands for maid table)
 mt = np.zeros(2)
-mt[ix(maid, 'murderer')] = 0.2
-mt[ix(maid, 'notmurderer')] = 0.8
+mt[maid.ix('murderer')] = 0.2
+mt[maid.ix('notmurderer')] = 0.8
 
 # p(knife|butler,maid) potential table (kt stands for knife table)
 kt = np.zeros((2, 2, 2))  # 2 states per variable, a dim per variable
-kt[ix(knife, 'used'), ix(butler, 'notmurderer'), ix(maid, 'notmurderer')] = 0.3
-kt[ix(knife, 'used'), ix(butler, 'notmurderer'), ix(maid, 'murderer')] = 0.2
-kt[ix(knife, 'used'), ix(butler, 'murderer'), ix(maid, 'notmurderer')] = 0.6
-kt[ix(knife, 'used'), ix(butler, 'murderer'), ix(maid, 'murderer')] = 0.1
+kt[knife.ix('used'), butler.ix('notmurderer'), maid.ix('notmurderer')] = 0.3
+kt[knife.ix('used'), butler.ix('notmurderer'), maid.ix('murderer')] = 0.2
+kt[knife.ix('used'), butler.ix('murderer'), maid.ix('notmurderer')] = 0.6
+kt[knife.ix('used'), butler.ix('murderer'), maid.ix('murderer')] = 0.1
 # Use broadcasting, Ellipsis and normalization to complete the table
-kt[ix(knife, 'notused'), ...] = 1 - kt[ix(knife, 'used'), ...]
+kt[knife.ix('notused'), ...] = 1 - kt[knife.ix('used'), ...]
 
 # Put together all the potential in a list. Need to put them in the correct
 # order. Currently it is hard coded.
 pot = [
-    Potential(variables=knife, table=kt),
-    Potential(variables=maid, table=mt),
-    Potential(variables=butler, table=bt)
+    Potential(variables=knife.index, table=kt),
+    Potential(variables=maid.index, table=mt),
+    Potential(variables=butler.index, table=bt)
 ]
-
 
 
 
