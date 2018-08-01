3be779c3b8a05eaca4c81dab3cce36dd943195eb
Mauro Camara Escudero <maurocamaraescudero@gmail.com>
2018-08-01 21:07:31 +0200

    file containing function to multiply probability tables together

---
+++ brml/multpots.py
@@ -0,0 +1,10 @@
+
+
+def multpots(pots):
+    """
+    Function that multiplies together the probability tables stored in an
+    iterable such as a list, or a numpy.array.
+    :param pots:
+    :return:
+    """
+    pass
