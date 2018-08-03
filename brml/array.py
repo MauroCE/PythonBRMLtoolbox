5bd151ac160a5b39e065cd2129d38b5c099aabae
Mauro Camara Escudero <maurocamaraescudero@gmail.com>
2018-08-03 12:51:00 +0200

    sum function to sum Array probability table over a set of variables

--- brml/array.py
+++ brml/array.py
@@ -1,3 +1,4 @@
+import copy
 import numpy as np
 from brml.utilities import isvector
 from brml.potential import Potential
@@ -14,6 +15,15 @@ class Array(Potential):
         """
         # Instantiate Potential class.
         super().__init__(variables, table)
+
+    def __len__(self):
+        """
+        Overwrites the behavior of the __len__ call (for instance len(Array).
+        When calling len(Array) it is equivalent to just call Array.size()
+        :return: Number of states in the Array's table
+        :rtype: numpy.array
+        """
+        return self.size()
 
     def to_logarray(self):
         """
@@ -43,6 +53,31 @@ class Array(Potential):
             return np.array([len(self.table)]).astype(np.int8)
         else:
             return np.array(self.table.shape).astype(np.int8)
+
+    def sum(self, variables):
+        """
+        This method sums the probability table over provided variables. It is
+        the marginalization operation.
+        The function brml.setminus is replicated by np.setdiff1d()
+        https://docs.scipy.org/doc/numpy-1.13.0/reference/generated/numpy.setdiff1d.html
+        The function brml.ismember is replicated by np.isin()
+        https://docs.scipy.org/doc/numpy-1.14.0/reference/generated/numpy.isin.html
+        :param variables:
+        :return:
+        """
+        # New Array will have variables in self that are not in variables
+        # because those will be summed over
+        newpot = Array(variables=np.setdiff1d(self.variables, variables))
+        # Find the indexes of the variables that we will sum over. These are
+        # all those in self that are in variables
+        to_sumover = np.isin(self.variables, variables).nonzero()
+        # Find the table
+        t = copy.deepcopy(self.table)
+        for variable_index in to_sumover:
+            t.sum(axis=variable_index)
+        # Add table to Array
+        newpot.set_table(table=t)
+        return newpot
 
     def sumpot(self, axis):
         """
